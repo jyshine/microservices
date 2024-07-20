4651eae90bef12cf78e98ead514287d98b4041c1
신준영 <jyshine3@gmail.com>
2024-07-21 03:15:33 +0900

    persistence:새 API 추가, java bean mapper 추가 , service 테스트(review) service 추가

--- microservices/product-composite-service/src/main/java/se/magnus/microservices/composite/product/services/ProductCompositeIntegration.java
+++ microservices/product-composite-service/src/main/java/se/magnus/microservices/composite/product/services/ProductCompositeIntegration.java
@@ -153,6 +153,22 @@ public class ProductCompositeIntegration implements ProductService, Recommendati
     }
   }
 
+  @Override
+  public Review createReview(Review body) {
+
+    try {
+      String url = reviewServiceUrl;
+      LOG.debug("Will post a new review to URL: {}", url);
+
+      Review review = restTemplate.postForObject(url, body, Review.class);
+      LOG.debug("Created a review with id: {}", review.getProductId());
+
+      return review;
+
+    } catch (HttpClientErrorException ex) {
+      throw handleHttpClientException(ex);
+    }
+  }
 
   @Override
   public List<Review> getReviews(int productId) {
@@ -174,7 +190,18 @@ public class ProductCompositeIntegration implements ProductService, Recommendati
     }
   }
 
+  @Override
+  public void deleteReviews(int productId) {
+    try {
+      String url = reviewServiceUrl + "?productId=" + productId;
+      LOG.debug("Will call the deleteReviews API on URL: {}", url);
 
+      restTemplate.delete(url);
+
+    } catch (HttpClientErrorException ex) {
+      throw handleHttpClientException(ex);
+    }
+  }
 
   private RuntimeException handleHttpClientException(HttpClientErrorException ex) {
     switch (HttpStatus.resolve(ex.getStatusCode().value())) {
